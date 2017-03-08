bb06d5fb923a894a0af51d855ba77ff94286966b
Bill Klinkow <klinkow@gmail.com>
2017-03-08 11:00:52 -0800

    add basic front end view for gameplay - questio#show and question controller developed.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,8 @@
 class PlayersController < ApplicationController
 
   def make_questions(player1, player2)
-    question1 = Question.create(question: "Is the capitol of Illinois Springfield?", answer_correct: "True", answer_1: "True", answer_2: "False", correct_response: "Well done.", wrong_response: "Terrible.", number: 1, topic: "geography", round: 1, player: player1.name, fitb: false, mc: false, tf: true, finished?: false)
-    question2 = Question.create(question: "Who framed roger rabit?", answer_correct: "Me", answer_1: "You", answer_2: "Me", answer_3: "Him", answer_4: "Her", correct_response: "Good Job.", wrong_response: "You suck.", number: 2, topic: "general knowledge", round: 1, player: player2.name, fitb: false, mc: true, tf: false, finished?: false)
+    question1 = Question.create(question: "Is the capitol of Illinois Springfield?", answer_correct: "True", answer_1: "True", answer_2: "False", correct_response: "Well done.", wrong_response: "Terrible.", number: 1, topic: "geography", round: 1, player: player1.name, fitb: false, mc: false, tf: true, finished: false)
+    question2 = Question.create(question: "Who framed roger rabit?", answer_correct: "Me", answer_1: "You", answer_2: "Me", answer_3: "Him", answer_4: "Her", correct_response: "Good Job.", wrong_response: "You suck.", number: 2, topic: "general knowledge", round: 1, player: player2.name, fitb: false, mc: true, tf: false, finished: false)
   end
 
   def index
@@ -10,16 +10,15 @@ class PlayersController < ApplicationController
 
   def new
     @player = Player.new
-    @number = params[:format]
+    @number = params[:format].to_i
   end
 
   def create
-    @player = Player.new(player_params.merge(score: 0, latest_score: 0, number: 1))
-    binding.pry
-    if params[:format] === 2
+    @player = Player.new(player_params.merge(score: 0, latest_score: 0))
+    if player_params[:number] === "2"
       @player.save
-      # make_questions(Player.find_by number: 1, @player)
-      redirect_to question_path(1)
+      make_questions((Player.find_by number: 1), @player)
+      redirect_to question_path(Question.find_by number:1)
     elsif @player.save
       redirect_to new_player_path(2)
     end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,19 @@
 class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
-    @player = Player.find_by number: @question.player
-    if params[:user_answer] === @question.answer_correct
-      @player.score += params[:correct_score]
-    else
-      @player.score += params[:incorrect_score]
+    @player = (Player.find_by name: @question.player)
+    nextquestion = Question.find_by number: (@question.number + 1)
+    if params[:amount_waged]
+      @question.correct_score = params[:amount_waged].to_i
+      @question.incorrect_score = -(params[:amount_waged].to_i)
+      redirect_to question_path(@question)
+    elsif params[:user_answer]
+      if params[:user_answer] === @question.answer_correct
+        @player.score += params[:correct_score].to_i
+      else
+        @player.score += params[:incorrect_score].to_i
+      end
+    redirect_to question_path(nextquestion)
     end
   end
 end
