072d722ae2e3a69df6c7dc11f3de806690733691
Bill Klinkow <klinkow@gmail.com>
2017-03-08 16:50:37 -0800

    add turn announcement to show page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-    @@player1 = Player.find_by number: 1
-    @@player2 = Player.find_by number: 2
-
-
 end
