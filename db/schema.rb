f1701d747a0dffe29810c3b80b4a48a73fb82c88
Bill Klinkow <klinkow@gmail.com>
2017-03-08 19:49:46 -0800

    add styling.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307195131) do
+ActiveRecord::Schema.define(version: 20170308000322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,12 @@ ActiveRecord::Schema.define(version: 20170307195131) do
     t.string   "wrong_response"
     t.integer  "number"
     t.string   "topic"
+    t.integer  "round"
     t.string   "player"
-    t.integer  "correct_score"
-    t.integer  "incorrect_score"
+    t.boolean  "fitb"
+    t.boolean  "mc"
+    t.boolean  "tf"
+    t.boolean  "finished"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
