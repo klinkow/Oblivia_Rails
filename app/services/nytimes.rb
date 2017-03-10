55e25259cee24bf2d2484456cd7be889101beab8
Bill Klinkow <klinkow@gmail.com>
2017-03-09 22:11:11 -0800

    fix typo

--- app/services/nytimes.rb
+++ app/services/nytimes.rb
@@ -1,6 +1,6 @@
 # class Nytimes
 #   require 'fuzzystringmatch'
-#   jarow = FuzzyStringMatch::JaroWinkler.create(:native)
+#   fuzzy = FuzzyStringMatch::JaroWinkler.create(:native)
 #
 #
 #
@@ -23,22 +23,20 @@
 #   def sort_us_politics(articles)
 #     results = []
 #     articles.each do |article|
-#       if result['section'] === "U.S." && result['subsection'] === "Politics" && jarow.getDistance(result['per_facet'].first(), 'johnson')
+#       if result['section'] === "U.S." && result['subsection'] === "Politics" && fuzzy.getDistance(result['per_facet'].first(), 'johnson')
 #
 #
 #
 #       require 'fuzzystringmatch'
-#       jarow = FuzzyStringMatch::JaroWinkler.create(:native)
+#       fuzzy = FuzzyStringMatch::JaroWinkler.create(:native)
 #
 #       headline = "Obama retires to Richard Branson's rich guy hideout."
 #       name = "Obama, Barack"
 #
-#       I want to test the last name of the first person against all words in the headline.  If it matches (is included in), I want to check to see if the word prior in the headline matches with fuzzy logic the first name.  If it matches I want to replace both words with a single blank.  The input from the user should only match the last name with fuzzy logic.  If the first name is not contained, it will just make a blank for hte last name.  Otherwise if there is no match whatsoever, I will check the next article.
-#
 #
 #       headlineWords = headline.split(" ")
 #       lastName = name.split(",").first()
-#       firstName
+#       # firstName = ...
 #       allNames = name.split('"')
 #
 #
@@ -67,7 +65,7 @@
 #
 #       headlineWords.foreach do |headline_word|
 #         nameArray.foreach do |name_word|
-#           if (jarow.getDistance(headline_word, name_word) > .8)
+#           if (fuzzy.getDistance(headline_word, name_word) > .8)
 #
 #
 #
